11c27e89ee0f85644da471c3a1956ff02b6a81ea
Rigidus <avenger-f@yandex.ru>
2012-01-07 03:34:16 +0400

    (menu)

--- init.lisp
+++ init.lisp
@@ -1,5 +1,14 @@
 (in-package #:svdba)
 
+(defun menu ()
+  (list (list :link "/" :title "Главная")
+        (list :link "/plan" :title "Планирование и бюджет")
+        (list :link "/whywe/" :title "Почему мы?")
+        (list :link "/santorini/" :title "Санторини")
+        (list :link "/gallery/" :title "Фотогалерея")
+        (list :link "/contaces/" :title "Свяжитесь с нами")))
+
+
 (restas:start '#:svdba :port 7071)
 
 (restas:debug-mode-on)
