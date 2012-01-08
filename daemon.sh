1b2516690fa114caeac6f75c85e93bfa2613157e
Rigidus <avenger-f@yandex.ru>
2012-01-08 05:58:50 +0400

    daemon correct path for username

--- daemon.sh
+++ daemon.sh
@@ -1,2 +1,1 @@
-# ../scripts/daemon.sh $(pwd)/example1.conf $1
-/home/rigidus/repo/daemonization/scripts/daemon.sh /home/rigidus/repo/svdba/daemon.conf $1
+$(pwd)/../daemonization/scripts/daemon.sh $(pwd)/daemon.conf $1
